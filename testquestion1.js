9396463138a91155aba47e4badca95ebab0adca7
Walden-pro <waldenchho@gmail.com>
2025-10-22 08:32:05 +0000

    endsem

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,29 @@
+let posX = 600/2;
+let xSpeed = 5;
+function setup(){
+    createCanvas(600, 400);
+    posX = width/ 2;
+    xSpeed = 5;
+    background("skyblue");
+fill('limegreen');
 
+}
+function draw(){
+background("skyblue");
+circle(posX,height/2,50);
+
+posX = posX + xSpeed;
+if (posX > 600){
+    xSpeed = xSpeed * -1;
+
+}else if (posX <0){
+    xSpeed = xSpeed * -1;
+}
+
+
+ 
+
+
+
+
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,29 @@
+let pokemons = ["Pikachu"
+    ,"Raichu"
+    ,"Bulbasaur"
+    ,"Mewtwo"
+    ,"Charizard"];
+let yPos;
 
+function setup(){
+    createCanvas(200, 300);
+    fill("pink");
+    // Task: Align the text to the center using textAlign
+    // Task: set font size to 20 using textSize
+    textAlign(CENTER,CENTER);
+    background("black");
+    textSize(20);
+    
+}
+function draw(){
+    // background("black")
+    yPos = 100;
+    fill("pink");
+for (let index = 0; index < pokemons.length; index++) {
+yPos = yPos +25;
+ text(pokemons[index],width/2,yPos);
+} 
+
+
+
+}
